3b801b6a960c469df7d01c3a44043394d3ad6ce2
maohw <476231140@qq.com>
2019-05-02 11:34:50 +0800

    可复制集架构的连接配置

--- dalomao-mongodb/src/main/java/com/dalomao/mongodb/quickstart/comment/config/MongoClientConfig.java
+++ dalomao-mongodb/src/main/java/com/dalomao/mongodb/quickstart/comment/config/MongoClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class MongoClientConfig {
@@ -23,13 +24,28 @@ public class MongoClientConfig {
 				.threadsAllowedToBlockForConnectionMultiplier(5)
 				.maxWaitTime(120000).connectTimeout(10000).build();
 
+		/**
+		 * 单机架构配置
+		 */
 	   MongoClient client = new MongoClient(new ServerAddress("192.168.31.41", 27017), mco);
 
 	   /**
-		密码方式配置：
+		* 密码方式配置：
+
 		MongoCredential createCredential = MongoCredential.createCredential("maohw", "maohw", "123456".toCharArray());
 		MongoClient client = new MongoClient(new ServerAddress("192.168.31.41", 27017), Arrays.asList(createCredential), mco);
 		*/
-	   return client;
+
+		/**
+		 * 可复制集架构配置：
+
+		 List<ServerAddress> asList = Arrays.asList(
+				new ServerAddress("192.168.1.142", 27018),
+				new ServerAddress("192.168.1.142", 27017),
+				new ServerAddress("192.168.1.142", 27019));
+		MongoClient client = new MongoClient(asList, mco);
+		 */
+
+		return client;
 	}
 }
